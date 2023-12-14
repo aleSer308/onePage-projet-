825c168864a47ab72e953e1e5eae88466813d715
aleSer308 <alegresergio@hotmail.com.ar>
2023-12-14 14:33:09 -0300

    primeros pasos

---
+++ porfolio/src/App.js
@@ -0,0 +1,11 @@
+import './App.css';
+import TopBar from './components/TopBar';
+
+
+function App() {
+  return (
+    <TopBar/>
+  );
+}
+
+export default App;
---
+++ porfolio/src/components/TopBar.jsx
@@ -0,0 +1,28 @@
+import Dropdown from 'react-bootstrap/Dropdown';
+import DropdownButton from 'react-bootstrap/DropdownButton';
+
+function TopBar(){
+    return(
+        <>
+        <nav>
+            <div class="logo">
+                <image class="logoImg"
+                src="../../assets/" alt="logo">
+            </div>
+            <div>
+                <ul>
+                    <li><a href=""></a></li>
+                    <li><a href=""></a></li>
+                    <li><a href=""></a></li>
+                </ul>
+            </div>
+            <div class="buttons">
+                <buttosn>algo</buttosn>
+                <buttosn>algo</buttosn>
+            </div>
+        </nav>
+        </>
+    )
+};
+
+export default TopBar;
